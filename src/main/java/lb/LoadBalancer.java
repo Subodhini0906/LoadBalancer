638b1eda819f7ba500c0cd0fad15b04234521ea7
Subodhini0906 <subodhiniaggarwal@gmail.com>
2025-06-16 23:03:25 +0530

    single backend completed

--- src/main/java/lb/LoadBalancer.java
+++ src/main/java/lb/LoadBalancer.java
@@ -1,30 +1,30 @@
-package lb;
+package lb; 
 
-import java.io.*;
-import java.net.*;
+import java.io.*;        //input/output streams 
+import java.net.*;       //netwrk clses (socket,serversocket)
 
-public class LoadBalancer {
-    public static void main(String[] args) throws IOException {
-        int port = 8088; // Load Balancer port
-        String backendHost = "localhost";
-        int backendPort = 8080; // Your backend server port
+public class LoadBalancer{
+    public static void main(String[] args) throws IOException{               //bcoz networking can thrw exceptions
+        int port=8088; //load balancer port(client send http requests to this port)
+        String backendHost="localhost";
+        int backendPort=8080;    //backnd server port
 
-        ServerSocket serverSocket = new ServerSocket(port);
-        System.out.println("Load Balancer running on port " + port);
+        ServerSocket serverSocket=new ServerSocket(port);       //opn srvr socket i.e. waits for incoming client connections
+        System.out.println("Load Balancer running on port"+port);
 
-        while (true) {
-            Socket clientSocket = serverSocket.accept();
-            new Thread(() -> handleRequest(clientSocket, backendHost, backendPort)).start();
+        while(true){
+            Socket clientSocket=serverSocket.accept();
+            new Thread(()-> handleRequest(clientSocket,backendHost,backendPort)).start();
         }
     }
 
     private static void handleRequest(Socket clientSocket, String backendHost, int backendPort) {
         try (
-            Socket backendSocket = new Socket(backendHost, backendPort);
-            InputStream clientIn = clientSocket.getInputStream();
-            OutputStream clientOut = clientSocket.getOutputStream();
-            InputStream backendIn = backendSocket.getInputStream();
-            OutputStream backendOut = backendSocket.getOutputStream()
+            Socket backendSocket = new Socket(backendHost, backendPort);       //connects to backend server
+            InputStream clientIn = clientSocket.getInputStream();              //read cient request
+            OutputStream clientOut = clientSocket.getOutputStream();           //send response
+            InputStream backendIn = backendSocket.getInputStream();            //read from backend
+            OutputStream backendOut = backendSocket.getOutputStream()          //sends to bcknd
         ) {
             clientIn.transferTo(backendOut);
             backendOut.flush();
